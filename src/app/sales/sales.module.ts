5b62dd130c292ffc1561f0eff8e56c888babad17
Raf ASSAS <riko714@gmail.com>
2021-07-25 18:42:37 +0000

    fix sales end

---
+++ src/app/sales/sales.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalesendComponent } from './salesend/salesend.component';
+
+import { MatTableModule } from '@angular/material/table' ;S
+
+@NgModule({
+  declarations: [
+    SalesendComponent
+  ],
+  imports: [
+    CommonModule,
+    MatTableModule
+  ],
+
+  exports: [
+    CommonModule,
+    SalesendComponent
+  ]
+})
+export class SalesModule { }
